5ff348216b927697211839b1350f343e97f87564
SergOswald <skobrusev446@gmail.com>
2024-06-01 15:30:24 +0300

    correct

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -6,8 +6,8 @@ import {
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
 
-// import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import * as navigation from 'next/navigation';
 import clsx from 'clsx';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -22,7 +22,7 @@ const links = [
 ];
 
 export default function NavLinks() {
-  const pathname = usePathname();
+  const pathname = navigation.usePathname();
   return (
     <>
       {links.map((link) => {
